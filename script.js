b12db6996007fb70d5c0421f9a40723f11721ef8
alzainalm <alzain2005alm@gmail.com>
2022-08-18 13:49:08 +0300

    cw7 done

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+function BMI ( weight, height) {
+let result = weight / ( height*height) 
+return result
 
+}
+console.log ( BMI(20,20))
+
+
+let result = document.getElementById('al')
+function Status (bmi) {
+if ( bmi < 18.5 ) { 
+    return ' لديك نقص في الوزن'
+    
+ 
+}
+else if ( bmi >= 18.5 && bmi < 25) {
+return ' وزنك صحي'
+
+}
+else if ( bmi >= 25) {
+return ' لديك زيادة في الوزن '
+
+}
+
+
+}
+ function calculate () {
+    let weight = document.getElementById('weight').value
+    let height = document.getElementById('height').value
+
+    let bmi = BMI(weight,height)
+    let desc = Status (bmi)
+
+    let div = document.getElementById('result')
+    
+  div.innerText = bmi + " == " + desc
+
+ }
